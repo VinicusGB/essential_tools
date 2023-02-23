0019060ffba9962fa90717d45962f01bb6f7c7c8
VinicusGB <viniciusgomesb@gmail.com>
2023-02-23 15:17:30 -0300

    refact: diretorios

--- diretorio/classe.py
+++ diretorio/classe.py
@@ -8,29 +8,29 @@ class Diretorio():
     def __init__(self,projeto=''):
         os.chdir(os.path.join(os.getcwd(),projeto))
         self.base = os.getcwd()
-        self.input = os.path.join(self.base,'inputs')
-        self.output = os.path.join(self.base,'outputs')
+        self.inputs = os.path.join(self.base,'inputs')
+        self.outputs = os.path.join(self.base,'outputs')
     def __str__(self):
         return self.base
     def make(self) -> None:
         """Cria as pastas input e output"""
-        return [os.makedirs(x) for x in [self.input,self.output]]
+        return [os.makedirs(x) for x in [self.input,self.outputs]]
     def input_list_files(self,e='',s='',p=False):
         """Retorna os arquvios da pasta input.\nFiltros: end = sufixo, start = prefixo, p = printa os valores na tela (True/False) """
         return [f"{os.listdir(self.input).index(filename)} - {filename}" if p == True else os.path.join(self.input,filename) for filename in os.listdir(self.input) if filename.endswith(e) and filename.startswith(s)]
     def output_list_files(self,e='',s='',p=False):
         """Retorna os arquvios da pasta output.\nFiltros: end = sufixo, start = prefixo, print = bool """
-        return [f"{os.listdir(self.output).index(filename)} - {filename}" if p == True else os.path.join(self.output,filename) for filename in os.listdir(self.output) if filename.endswith(e) and filename.startswith(s)]
+        return [f"{os.listdir(self.outputs).index(filename)} - {filename}" if p == True else os.path.join(self.outputs,filename) for filename in os.listdir(self.outputs) if filename.endswith(e) and filename.startswith(s)]
     def input_clear(self):
         """Limpa a pasta input"""
         return [os.remove(os.path.join(self.input,file)) for file in self.input_list_files()]
     def output_clear(self):
         """Limpa a pasta output"""
-        return [os.remove(os.path.join(self.output,file)) for file in self.output_list_files()]
+        return [os.remove(os.path.join(self.outputs,file)) for file in self.outputs_list_files()]
     def output_save_file(self,filename_output=datetime.today(),f='csv',t='w',e='utf-8',contexts=''):
         """Salva o arquivo na pasta de outputs. filename_output = nome do arquivo\nf = formato do arquivo final\nt = tipo de acesso ao arquivo (a - adiciona nova linha, w = sobreescreve o testo\ne = enconding)"""
         filename_output = str(filename_output).replace(' ','_').replace(':','_').replace('.','_')
-        with open(f"{os.path.join(self.output,filename_output)}.{f}",mode=t,encoding=e) as output_file:
+        with open(f"{os.path.join(self.outputs,filename_output)}.{f}",mode=t,encoding=e) as output_file:
             output_file.write(contexts)
     def input_read_files(self,files,num):
         #print('arquvios files')
